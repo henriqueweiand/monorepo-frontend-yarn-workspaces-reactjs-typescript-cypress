df3bd63cc967b50d2ac332872f3eac78b268d57e
henrique weiand <henriqueweiand@gmail.com>
2020-11-19 19:49:40 -0300

    fix

--- packages/apps/auth/src/components/forms/person/docs/FormPerson.stories.tsx
+++ packages/apps/auth/src/components/forms/person/docs/FormPerson.stories.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
-import { Theme } from "components";
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Theme } from '@monorepo/uikit'
 
-import Person from "../index";
-import { PersonProps } from "../types";
+import Person from '../index'
+import { PersonProps } from '../types'
 
 export default {
-  title: "Example/FormPerson",
-  component: Person,
-  argTypes: {
-    initialValues: {
-      type: { name: "object", required: false },
-      defaultValue: {
-        fullName: "Henrique Weiand",
-        document: "99999999999",
-      },
-      description: "Valores iniciais dos campos internos",
-    },
-  },
-} as Meta;
+	title: 'Example/FormPerson',
+	component: Person,
+	argTypes: {
+		initialValues: {
+			type: { name: 'object', required: false },
+			defaultValue: {
+				fullName: 'Henrique Weiand',
+				document: '99999999999'
+			},
+			description: 'Valores iniciais dos campos internos'
+		}
+	}
+} as Meta
 
-const Template: Story<PersonProps> = (args) => (
-  <Theme>
-    <Person {...args} />
-  </Theme>
-);
+const Template: Story<PersonProps> = args => (
+	<Theme>
+		<Person {...args} />
+	</Theme>
+)
 
-export const DefaultPerson = Template.bind({});
+export const DefaultPerson = Template.bind({})
